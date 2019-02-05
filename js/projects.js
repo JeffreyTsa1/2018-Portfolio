3240b208b520ef865da42cedce7b8e5a8475dd7d
Jeffrey Tsai <jeffreytsai@Jeffreys-MacBook-Pro.local>
2019-02-05 23:43:07 +0800

    Site Revision 2

--- js/projects.js
+++ js/projects.js
@@ -1,68 +1,200 @@
 var projects_arr_name = ["#hgtxt","#ttmtxt","#pevtxt","#aatxt"]; // Array of the ID for each project name
 var projects_arr_pic = ["#hgprv","#ttmprv","#pevprv","#aaprv"]; // Array of the ID of the imgs of projects
 
-var hgVis = false
-var ttmVis = false;
-var pevVis = false;
+var hgVis = false // HourGlass
+var ttmVis = false; // Tree Tab Manager
+var pevVis = false; // Twitter 2016 visualization
 var aaVis = false;
 
+$("#close").click(function(){
+	if(hgVis){
+		hgVis=false;
+		hg(1);
+		$("#ptxt").css("transform","translateY(0px)");
+		$("#close").css("transform","translateY(60px)");
+		$("#hourglass").removeClass("true");
+		$("#sDown").css("opacity","0");
+	}
+	if(ttmVis){
+		ttmVis=false;
+		ttm(1);
+		$("#ptxt").css("transform","translateY(0px)");
+		$("#close").css("transform","translateY(60px)");
+		$("#treetab").removeClass("true");
+		$("#sDown").css("opacity","0");
+	}
+	if(pevVis){
+		pevVis=false;
+		pev(1);
+		$("#ptxt").css("transform","translateY(0px)");
+		$("#close").css("transform","translateY(60px)");
+		$("#twitter_vis").removeClass("true");
+		$("#sDown").css("opacity","0");
+	}
+});
+
+function close(){
+
+}
+
 $("#hgtxt").hover(function() {
-	if(hgVis!=true){
-		hg(0);
+	if(!(ttmVis || pevVis || aaVis)){
+		if(hgVis!=true){
+			$("#hgprv").css("transform","translateY(0px)");
+			hg(0);
+		}
 	}
 },
 function(){
-	if(hgVis!=true){		
-		hg(1);
+	if(!(hgVis || pevVis || aaVis)){
+		if(hgVis!=true){
+			$("#hgprv").css("transform","translateY(10px)");		
+			hg(1);
+		}
 	}
 });
 
 $("#ttmtxt").hover(function() {
-	if(ttmVis!=true){		
-		ttm(0);
+	if(!(hgVis || pevVis || aaVis)){
+		if(ttmVis!=true){	
+			$("#ttmprv").css("transform","translateY(0px)");		
+			ttm(0);
+		}
+
 	}
-	else{
-		
-	}
+
 },
 function(){
-	if(ttmVis!=true){		
-		ttm(1);
+	if(!(hgVis || pevVis || aaVis)){
+		if(ttmVis!=true){		
+			$("#ttmprv").css("transform","translateY(10px)");	
+			ttm(1);
+		}
 	}
 });
 
-// $("#hgtxt").hover(function() {
-// 	hg(0);
-// },
-// function(){
-// 	hg(1);
-// });
+$("#pevtxt").hover(function() {
+	if(!(ttmVis || hgVis || aaVis)){
+		if(pevVis!=true){	
+			$("#pevprv").css("transform","translateY(0px)");	
+			pev(0);
+		}
+
+	}
+
+},
+function(){
+	if(!(hgVis || ttmVis || aaVis)){
+		if(pevVis!=true){
+			$("#pevprv").css("transform","translateY(10px)");	
+			pev(1);
+		}
+	}
+});
+
+function redirection(sectionVal){
+	if(hgVis){
+
+	}
+	else if(ttmVis){
+
+	}
+	else if(pevVis){
+
+	}
+	else if(aaVis){
+
+	}
+	else{
+		alert("error");
+	}
+
+}
+
 
 $("#hgtxt").click(function(){
+	if(ttmVis || pevVis || aaVis){
+		ttm(1);
+		$("#treetab").removeClass("true");
+		ttmVis=false;
+
+		$("#twitter_vis").removeClass("true");
+		pev(1);
+		pevVis=false;
+	}
 	if(hgVis==true){
 		hgVis=false;
 		hg(1);
-		$("#ptxt").css("opacity","1");
+		$("#ptxt").css("transform","translateY(0px)");
+		$("#close").css("transform","translateY(60px)");
 		$("#hourglass").removeClass("true");
+		$("#sDown").css("opacity","0");
 	}
-	else{
+	else{ // Case where it toggles
 		hgVis = true;
 		hg(0);
-		$("#ptxt").css("opacity","0");
+		$("#ptxt").css("transform","translateY(-60px)");
+		$("#close").css("transform","translateY(0px)");
 		$("#hourglass").addClass("true");
+		$("#sDown").css("opacity","1");
 	}
 });
 
 $("#ttmtxt").click(function(){
+	if(hgVis || pevVis || aaVis){
+		hg(1);
+		$("#hourglass").removeClass("true");
+		hgVis=false;
+
+		$("#twitter_vis").removeClass("true");
+		pev(1);
+		pevVis=false;
+	}
 	if(ttmVis==true){
 		ttmVis=false;
 		ttm(1);
-		$("#twitter_vis").removeClass("true");
+		$("#ptxt").css("transform","translateY(0px)");
+		$("#close").css("transform","translateY(60px)");
+		$("#treetab").removeClass("true");
+		$("#sDown").css("opacity","0");
 	}
 	else{
 		ttmVis = true;
 		ttm(0);
-		$("#twitter_vis").addClass("true");
+		$("#ptxt").css("transform","translateY(-60px)");
+		$("#close").css("transform","translateY(0px)");
+		$("#treetab").addClass("true");	
+		$("#sDown").css("opacity","1");
+
+	}
+});
+
+$("#pevtxt").click(function(){
+	if(hgVis || ttmVis || aaVis){
+		$("#hourglass").removeClass("true");
+		hg(1);
+		hgVis=false;
+
+		ttm(1);
+		$("#treetab").removeClass("true");
+		ttmVis=false;
+	}
+	if(pevVis==true){
+		pevVis=false;
+		pev(1);
+		$("#ptxt").css("transform","translateY(0px)");
+		$("#close").css("transform","translateY(60px)");
+		$("#twitter_vis").removeClass("true");
+		$("#sDown").css("opacity","0");
+	}
+	else{
+		pevVis = true;
+		pev(0);
+		$("#ptxt").css("transform","translateY(-60px)");
+		$("#close").css("transform","translateY(0px)");
+		$("#twitter_vis").addClass("true");	
+		$("#sDown").css("opacity","1");
+
 	}
 });
 
@@ -72,7 +204,7 @@ function hg(value){
 	// $("#pevtxt").css("opacity",value);
 	// $("#aatxt").css("opacity",value);
 	var val = parseInt(value,10);
-	$("#hgprv").css("opacity",val^1+"");	
+	$("#hgprv").css("opacity",val^1+"");
 }
 
 function ttm(value){
@@ -81,6 +213,14 @@ function ttm(value){
 	// $("#aatxt").css("opacity",value);
 	var val = parseInt(value,10);
 	$("#ttmprv").css("opacity",val^1+"");	
+}
+
+function pev(value){
+	// $("#hgtxt").css("opacity",value);
+	// $("#pevtxt").css("opacity",value);
+	// $("#aatxt").css("opacity",value);
+	var val = parseInt(value,10);
+	$("#pevprv").css("opacity",val^1+"");	
 }
 
 // function hg(value){
